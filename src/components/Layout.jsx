6d1d1abc5de18c4335d27c8be547caab22d5b80a
maxwin-sourcecode <maxwinsourcecode@gmail.com>
2024-08-05 12:13:31 +0630

    logo

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from "react";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
-import Navbar from "./Navbar";
-import FixedBottom from "./FixedBottom";
-import { Modal } from "react-bootstrap";
-import { CgClose } from "react-icons/cg";
-import useFetch from "../hooks/useFetch";
-import BASE_URL from "../hooks/baseURL";
+import React, { useEffect, useState } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import Navbar from './Navbar';
+import FixedBottom from './FixedBottom';
+import { Modal } from 'react-bootstrap';
+import { CgClose } from 'react-icons/cg';
+import useFetch from '../hooks/useFetch';
+import BASE_URL from '../hooks/baseURL';
 
 const Layout = () => {
   const location = useLocation();
   const [isModalOpen, setIsModalOpen] = useState(true);
-  const {data: ads} = useFetch(BASE_URL + '/popup-ads-banner');
+  const { data: ads } = useFetch(BASE_URL + '/popup-ads-banner');
   // console.log(ads);
 
-  const auth = localStorage.getItem("token");
+  const auth = localStorage.getItem('token');
 
   const lan = localStorage.getItem('lan');
-  const [language, setLanguage] = useState("english");
+  const [language, setLanguage] = useState('english');
   useEffect(() => {
     setLanguage(lan);
   }, [lan]);
@@ -28,37 +28,36 @@ const Layout = () => {
   //     navigate('/login');
   //   }, [navigate, auth]);
   // }
-  
+
   return (
     <div>
-      {location.pathname === "/" && (
-        <div className="welcomeText   text-center py-2 text-white">
-          {lan === "english" ? "Welcome To Super Bet" : "Super Bet မှ ကြိုဆိုပါတယ်။"}
+      {location.pathname === '/' && (
+        <div className='welcomeText   text-center py-2 text-white'>
+          {lan === 'english'
+            ? 'Welcome To Panda 666 Slot'
+            : 'Panda 666 Slot မှ ကြိုဆိုပါတယ်။'}
         </div>
       )}
       <Modal
-        className="adsModal"
+        className='adsModal'
         show={isModalOpen}
         onHide={() => setIsModalOpen(false)}
       >
         <Modal.Header>
           <Modal.Title
-            style={{ width: "100%" }}
-            className="  d-flex justify-content-end"
+            style={{ width: '100%' }}
+            className='  d-flex justify-content-end'
           >
             <div
               onClick={() => setIsModalOpen(false)}
-              className="modalCloseBtn cursor-pointer"
+              className='modalCloseBtn cursor-pointer'
             >
-              <CgClose color="black" />
+              <CgClose color='black' />
             </div>
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <img
-            src={ads?.img_url}
-            className="popUpImg"
-          />
+          <img src={ads?.img_url} className='popUpImg' />
         </Modal.Body>
       </Modal>
       <Navbar />
